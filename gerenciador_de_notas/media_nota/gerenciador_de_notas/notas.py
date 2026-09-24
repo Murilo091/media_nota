1517f9eeb089d7c700c874b51210a3b32d673ece
Murilo <delaramartinsmurilo@gmail.com>
2026-09-24 09:38:03 -0300

    Add files via upload

---
+++ gerenciador_de_notas/media_nota/gerenciador_de_notas/notas.py
@@ -0,0 +1,12 @@
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+
+media = (nota1 + nota2 + nota3) / 3
+
+print(f"A média final é: {media}")
+
+if media >= 7.0:
+    print("Voce foi: Aprovado")
+else:
+    print("Voce foi: Reprovado") 
